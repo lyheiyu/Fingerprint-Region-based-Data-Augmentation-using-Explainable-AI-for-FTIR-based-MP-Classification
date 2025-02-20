52aac5b91d3b564759c82f4de7bf12a7b2ac8816
Xinyu Yan <44694067+lyheiyu@users.noreply.github.com>
2025-02-20 12:05:26 +0000

    Update utils.py
    print scores

--- utils.py
+++ utils.py
@@ -23,8 +23,12 @@ class utils:
 
         scoreList=[]
         for score in scores:
-            scoreList.append(score(y_true,y_pre))
-            print(score.__name__,score(y_true,y_pre))
+         # if score is f1, recall, accuracy set the average to macro
+            if score.__name__ == 'recall_score' or score.__name__ == 'precision_score' or score.__name__ == 'f1_score':
+
+                scoreList.append(score(y_true,y_pre,average='macro'))
+            else: scoreList.append(score(y_true,y_pre))
+            # print(score.__name__,score(y_true,y_pre))
         return scoreList
     def mkdir(path):
         # 判断目录是否存在
@@ -133,7 +137,7 @@ class utils:
                  }
         font3 = {'family': 'Times New Roman',
                  'weight': 'normal',
-                 'size': 20,
+                 'size': 15,
                  }
         plt.imshow(cm, interpolation='nearest')  # 在特定的窗口上显示图像
         plt.title(title,font2)  # 图像标题
@@ -144,7 +148,7 @@ class utils:
         for x_val, y_val in zip(x.flatten(), y.flatten()):
             c = cm[y_val][x_val]
             if (c > 0.01):
-                plt.text(x_val, y_val, "%0.2f" % (c,), color='red', fontdict=font3, va='center', ha='center')
+                plt.text(x_val, y_val, "%0.2f" % (c,), color='red', va='center', ha='center',fontdict= font3)
 
         num_local = np.array(range(len(labels_name)))
         plt.xticks(num_local, labels_name, rotation=90,font=font2)  # 将标签印在x轴坐标上
@@ -165,11 +169,12 @@ class utils:
         #     intensityeach = []
         #     item2 = item[::-1]
         #     for item3 in item2:
-        #         if 100 - item3 == 100:
+        #         if 100 - item3>=100:
         #             intensityeach.append(0)
         #
         #         else:
         #             intensityeach.append(100 - item3)
+        #             print(max(intensityeach))
         #
         #     intensity2.append(intensityeach)
         intensity2=np.array(intensity)
@@ -221,6 +226,75 @@ class utils:
 
         return polymerName, waveLength, intensity, polymerID
 
+    def parseDataForSecondDataset2(fileName):
+        dataset = pd.read_csv(fileName, header=None, encoding='latin-1', keep_default_na=False, low_memory=False)
+        # print(dataset)
+        polymerName = dataset.iloc[1:, 0]
+        waveLength = dataset.iloc[0, 1:-1]
+        intensity = dataset.iloc[1:, 1:-1]
+        polymerName = np.array(polymerName)
+        intensity = np.array(intensity, dtype=np.float32)
+        intensity2 = []
+        for item in intensity:
+            intensityeach = []
+            item2 = item[::-1]
+            for item3 in item2:
+                if 100 - item3<=0:
+                    intensityeach.append(0)
+                    # print(100-item3)
+                else:
+                    intensityeach.append(100 - item3)
+
+
+            intensity2.append(intensityeach)
+        intensity2=np.array(intensity2)
+        intensityBaseline = []
+        for item in intensity2:
+            item = item - airPLS(item)
+            intensityBaseline.append(item)
+        # ss=StandardScaler()
+        # intensity=ss.fit_transform(intensity)
+        #intensity=intensity-airPLS(intensity)
+        PN=[]
+        for item in polymerName:
+            if item not in PN:
+                PN.append(item)
+        # scaler= StandardScaler().fit(intensityBaseline)
+        # intensity=scaler.transform(intensityBaseline)
+        # minScaler = MinMaxScaler().fit(intensityBaseline)
+        # intensity= minScaler.transform(intensityBaseline)
+        intensity = normalize(intensityBaseline, 'max')
+        #polymerID = dataset.iloc[1:, 1764]
+        polymerID = dataset.iloc[1:, -1]
+        polymerID1 = []
+        for item in polymerID:
+            polymerID1.append(int(item))
+        polymerID = np.array(polymerID1)
+
+        # polymerID1.append('polyID')
+        # for item in polymerName:
+        #     for i in range(len(PN)):
+        #         if item==PN[i]:
+        #             polymerID1.append(i)
+        # print(polymerID1)
+        # dataset['3552']=polymerID1
+        # dataset.to_csv('new_SecondDataset.csv')
+
+        #polymerID= int(polymerID)-np.ones(polymerID.shape[0])
+
+        # x_class = []
+        # y_class = []
+        # for i in range(12):
+        #     m = []
+        #     z = []
+        #     for j in range(len(intensity)):
+        #         if int(polymerID[j]) == i:
+        #             z.append(polymerID[j])
+        #             m.append(intensity[j])
+        #     x_class.append(m)
+        #     y_class.append(z)
+
+        return polymerName, waveLength, intensity, polymerID
     def getPN(fileName):
         dataset = pd.read_csv(fileName, header=None, encoding='latin-1', keep_default_na=False, low_memory=False)
 
@@ -346,7 +420,7 @@ class utils:
 
         # intensity= intensity.replace([np.inf, -np.inf], 0)
 
-        polymerID = np.array(polymerID,dtype=np.int)
+        polymerID = np.array(polymerID,dtype=np.int32)
         waveLength = dataset.iloc[0, :-2]
         intensity = np.array(intensity)
         waveLength = waveLength[::-1]
